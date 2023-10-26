37ed3d423e3684abd723b491df069af48f092c50
firstdray <142772338+firstdray@users.noreply.github.com>
2023-10-26 14:02:50 +0400

    Create Greeter.sol

---
+++ Greeter.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+contract Greeter {
+    
+    function CheckString(string memory _str) public pure returns(string memory){
+        return _str;
+    }
+}
